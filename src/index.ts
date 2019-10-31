10d803ca94a2b65b3ceb181ece9772578c92ba3a
Christopher Vizcarra <christopher.vizcarra@lendi.com.au>
2019-11-01 10:21:36 +1100

    Remove setting aws-sdk region & Simplify tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
 import * as yargs from "yargs";
-import * as AWS from "aws-sdk";
 import * as dotenv from "dotenv";
 
 dotenv.config();
-// set the default region
-AWS.config.update({ region: process.env.AWS_REGION });
 
 // tslint:disable-next-line:no-unused-expression
 yargs
