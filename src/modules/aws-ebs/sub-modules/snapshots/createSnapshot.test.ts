81593a538a342009c01d6425d4a8ded5e4b00506
Christopher Vizcarra <9291322+TigerTopher@users.noreply.github.com>
2019-12-06 17:28:44 +1100

    Add unit tests for createSnapshot and copySnapshotToTargetRegion

---
+++ src/modules/aws-ebs/sub-modules/snapshots/createSnapshot.test.ts
@@ -0,0 +1,37 @@
+import * as AWS from "aws-sdk";
+import * as AWSMock from "aws-sdk-mock";
+import * as sinon from "sinon";
+import { createSnapshot } from "./createSnapshot";
+
+AWSMock.setSDKInstance(AWS);
+
+describe("createSnapshot()", () => {
+  afterEach(() => {
+    AWSMock.restore("EC2");
+    sinon.restore();
+  });
+
+  it("should return SnapshotId when createSnapshot call is successful", async () => {
+    const volumeId = "vol-000";
+    const returnValue = "snap-000";
+
+    const spyCreateSnapshot = sinon.stub();
+    spyCreateSnapshot.resolves({
+      SnapshotId: returnValue,
+    });
+    AWSMock.mock("EC2", "createSnapshot", spyCreateSnapshot);
+
+    expect(await createSnapshot(volumeId)).toEqual(returnValue);
+  });
+
+  it("should throw an error if createSnapshot did not return a SnapshotId", async () => {
+    const volumeId = "vol-000";
+    const expectedErrorMessage = "No SnapshotId returned when creating snapshot.";
+
+    const spyCreateSnapshot = sinon.stub();
+    spyCreateSnapshot.resolves({});
+    AWSMock.mock("EC2", "createSnapshot", spyCreateSnapshot);
+
+    expect(createSnapshot(volumeId)).rejects.toEqual(new Error(expectedErrorMessage));
+  });
+});
